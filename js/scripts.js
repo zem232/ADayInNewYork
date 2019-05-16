28333d57387dc31baa4496d21d13a359b8ee5de9
zem232 <zem232@nyu.edu>
2019-05-16 11:52:36 -0400

    Push html

---
+++ js/scripts.js
@@ -0,0 +1,83 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiemVtMjMyIiwiYSI6ImNqdWQ5NXQxcDAydWw0NHBleGlnbDQ2NWIifQ.xzxdaO_DvGxl4eNCuIZ-Zg';
+var map = new mapboxgl.Map({
+  container: 'mapContainer',
+  style: 'mapbox://styles/mapbox/light-v10',
+  center: [-73.950348, 40.733210],
+  zoom: 11
+});
+
+
+var zoomThreshold = 4;
+
+// Created a sliding time scale for the user to select a time of day
+// https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_js_rangeslider
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+output.innerHTML = this.value;
+};
+
+
+map.on('load', function() {
+
+  $.getJSON('Data/trump_Elec_DayAfter.geojson', function(data) {
+    data.features.map(function(feature) {
+      //feature.properties.POPULATION = parseInt(feature.properties.POPULATION);
+    });
+
+    map.addSource('NTA-311-Complaints', {
+      type: 'geojson', // other types of sources include: video, vector tile, & others
+      data: data,
+    });
+
+    map.addLayer({
+        id: '311-complaints',
+        type: 'fill',
+        source: 'NTA-311-Complaints',
+        paint: {
+          'fill-opacity': 0.7,
+          'fill-color': [
+              'interpolate',
+              ['linear'],
+              ['get', 'count'],
+              0, '#fff7ec',
+              10, '#fee8c8',
+              20, '#fdd49e',
+              30, '#fdbb84',
+              40, '#fc8d59',
+              50, '#ef6548',
+              60, '#d7301f',
+              70, '#990000'
+
+          ],
+        }
+      });
+});
+
+  var popup = new mapboxgl.Popup({
+    closeButton: false,
+    closeOnClick: false
+  });
+});
+
+
+
+$(document).ready(function () {
+    $("#sidebar").mCustomScrollbar({
+        theme: "minimal"
+    });
+
+    $('#dismiss, .overlay').on('click', function () {
+        $('#sidebar').removeClass('active');
+        $('.overlay').removeClass('active');
+    });
+
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').addClass('active');
+        $('.overlay').addClass('active');
+        $('.collapse.in').toggleClass('in');
+        $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+    });
+});
